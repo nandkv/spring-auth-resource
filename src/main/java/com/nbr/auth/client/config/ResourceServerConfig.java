38664a9b764a8fa09758f17194e00f1d374943e1
nand <nandkv.online@gmail.com>
2018-02-08 22:14:24 -0600

    Additional Changes

---
+++ src/main/java/com/nbr/auth/client/config/ResourceServerConfig.java
@@ -0,0 +1,12 @@
+package com.nbr.auth.client.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated();
+	}
+}
